85bc3331bdcf408da6844059f7e991c265b02061
junsakamaki <junmoco13@icloud.com>
2022-07-20 17:04:08 +0900

    ポートフォリオサイトの作成を進めました。

---
+++ main.js
@@ -0,0 +1,15 @@
+const pictures = document.getElementById("pictures");
+const pic = ["Jun1.jpg", "Jun2.jpg", "Jun3.jpg", "Jun4.jpg"];
+
+const img_element = document.createElement("img");
+
+let i = 0;
+const changePic = function () {
+  pictures.src = pic[i];
+  i += 1;
+  if (i === pic.length) {
+    i = 0;
+  }
+};
+setInterval(changePic, 4000); /* 4秒ごとに繰り返す */
+changePic();
